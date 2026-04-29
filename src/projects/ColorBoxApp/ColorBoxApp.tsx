93e301de31ff1e57a2f8d9020767e32c5740cba3
wwwbrunagon <wwwbrunagon@gmail.com>
2026-04-29 17:44:30 -0300

    feat: Color Box project using a state-driven architecture with React + TypeScript

--- src/projects/ColorBoxApp/ColorBoxApp.tsx
+++ src/projects/ColorBoxApp/ColorBoxApp.tsx
@@ -1,5 +1,12 @@
+import ColorGrid from './ColorGrid';
+
 export default function ColorBoxApp() {
-	<div className="p-6">
-		<h1 className="text-2xl font-semibold mb-4">Color Box</h1>
-	</div>;
+	return (
+		<div className="min-h-screen flex flex-col items-center justify-center">
+			<div className="p-6">
+				<h1 className="text-2xl font-semibold mb-4">Color Box</h1>
+				<ColorGrid size={42} />
+			</div>
+		</div>
+	);
 }
