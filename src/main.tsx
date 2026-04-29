a24fb761b0024d075a002882b48f84e0219a310c
wwwbrunagon <wwwbrunagon@gmail.com>
2026-04-29 10:46:05 -0300

    install Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './main.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<h1 className="text-3xl font-bold text-blue-500">Tailwind is working</h1>
+		<App />
+	</StrictMode>,
+);
